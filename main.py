963cdad0d5e51e8bd22996c3913568ccfe96758a
arif <arifur.tuc@gmail.com>
2023-11-24 16:58:48 +0100

    Add URL scraping and data extraction function

--- main.py
+++ main.py
@@ -5,10 +5,19 @@ URL = "https://programmer100.pythonanywhere.com/tours/"
 
 
 def scrape(url):
-    """Scrape the page source from URL"""
+    """Scrape the page source from URL."""
     response = requests.get(url)
     source = response.text
     return source
 
 
-print(scrape(URL))
+def extract(source):
+    """Extract relevant data using a SelectorLib extractor configuration."""
+    extractor = selectorlib.Extractor.from_yaml_file("extract.yaml")
+    value = extractor.extract(source)["tours"]
+    return value
+
+
+scraped = scrape(URL)
+extracted = extract(scraped)
+print(extracted)
