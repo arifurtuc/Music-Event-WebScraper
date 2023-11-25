4a916237f8c4d003cc51d48c0c9e8be6820d074b
arif <arifur.tuc@gmail.com>
2023-11-25 16:12:21 +0100

    Store data in a text file if there is a new tour

--- main.py
+++ main.py
@@ -18,6 +18,31 @@ def extract(source):
     return value
 
 
+def send_email():
+    """Dummy function! Will be updated later with email sending
+    functionality"""
+    print("Email was sent!")
+
+
+def store(extracted):
+    """Store extracted data into a text file."""
+    with open("data.txt", "a") as file:
+        file.write(extracted + "\n")
+
+
+def read():
+    """Read and return the content from the data.txt file."""
+    with open("data.txt", "r") as file:
+        return file.read()
+
+
+# Scrape the website and extract tour information
 scraped = scrape(URL)
 extracted = extract(scraped)
-print(extracted)
+content = read()
+
+# Check for new upcoming tours, store and send an email if found
+if extracted.lower() != "no upcoming tours":
+    if extracted not in content:
+        store(extracted)
+        send_email()
